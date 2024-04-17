dec6ecd0a6c8f23d1eff2938a32522be97496b3e
harshiniryali <145111380+harshiniryali@users.noreply.github.com>
2024-04-17 18:48:37 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,29 @@
+import re
+def check_password_complexity(password):
+    length_criteria=len(password)>=8
+    uppercase_criteria=bool(re.search(r'[A-Z]',password))
+    lowercase_criteria=bool(re.search(r'[a-z]',password))
+    digit_criteria=bool(re.search(r'\d',password))
+    special_char_criteria=bool(re.search(r'[!@#$%^&*(),.?":{}|<>]',password))
+    if length_criteria and uppercase_criteria and lowercase_criteria and digit_criteria and special_char_criteria:
+        return "Strong password!!"
+    else:
+        feedback="Weak password. Consider the following improvements:\n"
+        if not length_criteria:
+            feedback+="- Ensure the password is atleast 8 characters long\n"
+        if not uppercase_criteria:
+            feedback+="- Include atleast one uppercase letter\n"
+        if not lowercase_criteria:
+            feedback+="- Include atleast one lowercase letter\n"
+        if not digit_criteria:
+            feedback+="- Include atleast one digit\n"
+        if not special_char_criteria:
+            feedback+="- Include atleast one special character\n"
+        return feedback
+def main():
+    print("Password Complexity Checker")
+    password=input("Enter your password: ")
+    result=check_password_complexity(password)
+    print(result)
+if __name__=="__main__":
+    main()
